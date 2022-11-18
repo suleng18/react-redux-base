7f0e160e0a53b25e721fff8972179a339af37fb6
Suleng18 <90331457+suleng18@users.noreply.github.com>
2022-11-18 14:59:52 +0700

    vid 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,17 @@
+// class component
+// function component
+
+import React from 'react';
+class MyComponent extends React.Component {
+  // JXS
+  render() {
+    return (
+      <div>
+        {Math.random()}
+        <h1>This is Component</h1>
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
